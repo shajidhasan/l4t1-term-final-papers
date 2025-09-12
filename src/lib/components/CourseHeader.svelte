5224ab333fe5ea948ca6fb47ba3389db81a2b620
shajidhasan <shajid6063@gmail.com>
2025-09-12 20:16:10 +0600

    Use shared component CourseHeader.svelte

--- src/lib/components/CourseHeader.svelte
+++ src/lib/components/CourseHeader.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <div
-	class="bg-surface-600-400/20 sticky top-0 right-0 left-0 z-100 flex items-center px-6 py-6 text-xs backdrop-blur-2xl"
+	class="bg-surface-600-400/20 sticky top-0 right-0 left-0 z-100 flex items-center p-4 text-xs backdrop-blur-2xl md:p-6"
 >
 	<span class="badge {getGradientForString(course.code)}">
 		{course.code}
@@ -16,7 +16,8 @@
 	<span class="ml-2 line-clamp-1 flex-1">
 		{course.title}
 	</span>
-	<a href="/" class="btn btn-icon hover:preset-tonal">
-		<Home />
+	<a href="/" class="btn btn-sm hover:preset-tonal">
+		<Home class="size-4" />
+		<span class="hidden sm:inline"> Home </span>
 	</a>
 </div>
--- src/routes/[courseCode]/+page.svelte
+++ src/routes/[courseCode]/+page.svelte
@@ -12,6 +12,7 @@
 	import { courses } from '$lib/courses';
 	import { getAvailableBatches } from '$lib/papers';
 	import { getGradientForString } from '$lib/utilities';
+	import CourseHeader from '$lib/components/CourseHeader.svelte';
 
 	const courseCode = $derived(page.params.courseCode as string);
 	const course = $derived(courses.find((c) => c.code === courseCode));
@@ -33,20 +34,7 @@
 </svelte:head>
 
 {#if course}
-	<div
-		class="bg-surface-600-400/20 sticky top-0 right-0 left-0 z-100 flex items-center px-6 py-6 text-xs backdrop-blur-2xl"
-	>
-		<span class="badge {getGradientForString(course.code)}">
-			{course.code}
-		</span>
-		<span class="ml-2 line-clamp-1 flex-1">
-			{course.title}
-		</span>
-
-		<a href="/" class="btn btn-icon hover:preset-tonal">
-			<Home />
-		</a>
-	</div>
+	<CourseHeader {course} />
 
 	<div class="p-4 md:p-6">
 		<Tabs contentBase="mt-10" value={group} onValueChange={(e) => (group = e.value)} fluid>
