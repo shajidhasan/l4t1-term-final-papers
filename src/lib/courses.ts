6043ec9c4c21c4f5a9c170fc462f4fbaa907b99d
shajidhasan <shajid6063@gmail.com>
2025-09-16 21:21:22 +0600

    Order courses as per schedule

--- src/lib/courses.ts
+++ src/lib/courses.ts
@@ -2,6 +2,24 @@ import type { Course } from "$lib/types";
 
 export const courses: Course[] = [
     {
+        title: 'Machine Tools and Tool Engineering',
+        code: 'ME451',
+        credits: 4,
+        instructors: 'Dr. M. A. Rahman, R. M. Meraz'
+    },
+    {
+        title: 'Production and Operations Management',
+        code: 'ME461',
+        credits: 4,
+        instructors: 'Dr. S. Rabbi, Dr. M. Hossain'
+    },
+    {
+        title: 'Machine Design II',
+        code: 'ME431',
+        credits: 3,
+        instructors: 'S. M. Ahmmad, M. Ahmed'
+    },
+    {
         title: 'Power Plant Engineering',
         code: 'ME413',
         credits: 3,
@@ -12,23 +30,5 @@ export const courses: Course[] = [
         code: 'ME417',
         credits: 3,
         instructors: 'Dr. J. U. Ahamed, M. A. Islam'
-    },
-    {
-        title: 'Production and Operations Management',
-        code: 'ME461',
-        credits: 4,
-        instructors: 'Dr. S. Rabbi, Dr. M. Hossain'
-    },
-    {
-        title: 'Machine Tools and Tool Engineering',
-        code: 'ME451',
-        credits: 4,
-        instructors: 'Dr. M. A. Rahman, R. M. Meraz'
-    },
-    {
-        title: 'Machine Design II',
-        code: 'ME431',
-        credits: 3,
-        instructors: 'S. M. Ahmmad, M. Ahmed'
     },
 ];
